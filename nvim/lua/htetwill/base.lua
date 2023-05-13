e1a710ac128b0ed8b960a678a349b3ecf42bc554
hw <htet.will@icloud.com>
2023-05-13 16:07:05 +0700

    replace NVIM's package manager packer->dein.nvim

---
+++ nvim/lua/htetwill/base.lua
@@ -0,0 +1,8 @@
+-- added by hw : Tab set to two spaces
+print('base lua reloaded!')
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.signcolumn = 'yes'
+vim.opt.termguicolors = true
---
+++ nvim/lua/htetwill/plugins.lua
@@ -0,0 +1,60 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Colorscheme
+  use 'folke/tokyonight.nvim'
+
+  -- LSP
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {                                      -- Optional
+        'williamboman/mason.nvim',
+        run = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},     -- Required
+      {'hrsh7th/cmp-nvim-lsp'}, -- Required
+      {'L3MON4D3/LuaSnip'},     -- Required
+    }
+  }
+end)
+
+-- add config lsp-zero
+local lsp = require('lsp-zero').preset({
+  name = 'minimal',
+  set_lsp_keymaps = true,
+  manage_nvim_cmp = true,
+  suggest_lsp_servers = false,
+})
+
+lsp.on_attach(function(client,bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+lsp.nvim_workspace()
+
+lsp.setup()
+
+pcall(vim.cmd, 'colorscheme tokyonight')
